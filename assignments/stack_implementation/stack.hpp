9c2ca59f28d0355cd1ce1095a4c290c2262f4b5c
Sesha Sai Behara <39871353+seshasaibehara@users.noreply.github.com>
2020-02-20 15:04:22 -0800

    Update

--- assignments/stack_implementation/stack.hpp
+++ assignments/stack_implementation/stack.hpp
@@ -4,7 +4,7 @@ class IntStack
     public:
         /// Returns true if there are no elements on the stack
         bool empty() const{
-            if (m_vector.size()==0){
+            if (size==0){
                 return true;
             }
             return false;
@@ -12,12 +12,12 @@ class IntStack
         
         /// Returns the number of elements on the stack
         int size() const{
-            return m_vector.size();
+            return size;
         }
 
         /// Return the top element of the stack, i.e. the last element that was pushed
         int top() const{
-            return m_vector[m_vector.size()-1];
+            return m_vector[size-1];
         }
 
         /// Adds a new element to the top of the stack
@@ -32,4 +32,5 @@ class IntStack
 
     private:
         std::vector<int> m_vector;
+        int size = m_vector.size();
 };
