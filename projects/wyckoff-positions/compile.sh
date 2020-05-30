51af674e2d10e2bd75a17b0773fe9306119e86c7
rcolleen <creynolds@ucsb.edu>
2020-05-30 14:04:48 -0700

    test compile script

---
+++ projects/wyckoff-positions/compile.sh
@@ -0,0 +1,1 @@
+g++ --std=c++17 ../avdv-factor-group/lattice.cpp ../avdv-factor-group/site.cpp ../avdv-factor-group/coordinate.cpp ../avdv-factor-group/structure.cpp ../avdv-factor-group/symop.cpp ../avdv-factor-group/fastsymmetry.cpp ../avdv-factor-group/point_group.cxx ../avdv-factor-group/factor_group.cpp ../avdv-factor-group/tests.cpp ./wyckoff.cpp ./wyckoff_test.cpp -o wyckoff_test
