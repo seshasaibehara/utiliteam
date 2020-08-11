80e16a2dde0c19902d71047c481ccd74d18dd636
seshasaibehara <seshasaibehara@gmail.com>
2020-08-11 12:13:24 -0700

    wrote symop 4d tests

--- projects/wyckoff-positions/symop_4d.cpp
+++ projects/wyckoff-positions/symop_4d.cpp
@@ -1,61 +1,69 @@
-#include "symopd_4d.hpp"
+#include "symop_4d.hpp"
 #include "wyckoff.hpp"
 
-Symop_4d::Symop_4d(const Eigen::Matrix4d& input_matrix):symop_matrix(input_matrix)
-{ 
+Symop_4d::Symop_4d(const Eigen::Matrix4d& input_matrix) : symop_matrix(input_matrix) {}
 
-}
-
-Symop_4d::Symop_4d(const Eigen::Matrix3d& cart_matrix, const Eigen::Vector3d& translation){
-    Eigen::Matrix4d symop_matrix=Eigen::Matrix4d::Zero();
-    for(int i=0; i<3; i++){
-        for (int j =0; j<3; j++){
-            symop_matrix(i,j)=cart_matrix(i,j);
+Symop_4d::Symop_4d(const Eigen::Matrix3d& cart_matrix, const Eigen::Vector3d& translation)
+{
+    Eigen::Matrix4d symop_matrix = Eigen::Matrix4d::Zero();
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            symop_matrix(i, j) = cart_matrix(i, j);
         }
-        symop_matrix(i,3)=translation(i);
+        symop_matrix(i, 3) = translation(i);
     }
-    symop_matrix(3,3)=1;
-    Eigen::Matrix3d empty_subspace=Eigen::Matrix3d::Zero();
-    Eigen::Vector3d empty_offset=Eigen::Vector3d::Zero();
+    symop_matrix(3, 3) = 1;
+    Eigen::Matrix3d empty_subspace = Eigen::Matrix3d::Zero();
+    Eigen::Vector3d empty_offset = Eigen::Vector3d::Zero();
 }
 
-Symop_4d::Symop_4d(const SymOp& symop_3d){
-    Symop_4d(symop_3d.get_cart_matrix(), symop_3d.get_translation());
-}
+Symop_4d::Symop_4d(const SymOp& symop_3d) { Symop_4d(symop_3d.get_cart_matrix(), symop_3d.get_translation()); }
 
 Subspace Symop_4d::find_invariant_subspace()
-{   double tol=1e-6;
-    if (dimension==-1)
+{
+    double tol = 1e-6;
+    if (dimension == -1)
     {
-        Eigen::Matrix3d basis_matrix=Eigen::Matrix3d::Zero();
-        Eigen::Vector3d offset=Eigen::Vector3d::Zero();
+        Eigen::Matrix3d basis_matrix = Eigen::Matrix3d::Zero();
+        Eigen::Vector3d offset = Eigen::Vector3d::Zero();
         Eigen::EigenSolver<Eigen::Matrix4d> solver(symop_matrix, true);
-        Eigen::Vector4<std::complex<double>>eigenvals=solver.eigenvalues();
+        Eigen::Vector4<std::complex<double>> eigenvals = solver.eigenvalues();
         std::vector<Eigen::Vector4d> output_eigen_vectors;
         Eigen::Matrix4<std::complex<double>> eigenvectors = solver.eigenvectors();
         dimension++;
-        for (int i=0; i<4;i++)
+        for (int i = 0; i < 4; i++)
         {
-            if ((1-abs(eigenvals(i).real()))<tol)
-            { if (1-abs(eigenvectors.col(i)(3))<tol)
-                {   for (int k=0; k<3; k++)
-                    {offset(k)=eigenvectors.col(i)(k).real();}
+            if ((1 - abs(eigenvals(i).real())) < tol)
+            {
+                if (1 - abs(eigenvectors.col(i)(3)) < tol)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        offset(k) = eigenvectors.col(i)(k).real();
+                    }
                 }
-              else{ for (int k=0; k<3; k++)
-                    {basis_matrix(k,dimension)=eigenvectors(k,i).real();}
-                    dimension++;}
+                else
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        basis_matrix(k, dimension) = eigenvectors(k, i).real();
+                    }
+                    dimension++;
+                }
             }
         }
-        invariant_subspace= Subspace(basis_matrix, offset);
-    
+        invariant_subspace = Subspace(basis_matrix, offset);
     }
     return this->invariant_subspace;
 }
 
 int Symop_4d::subspace_dimension()
 {
-    if (dimension==1)
-    { this->find_invariant_subspace();}
+    if (dimension == 1)
+    {
+        this->find_invariant_subspace();
+    }
     return this->dimension;
 }
-
--- projects/wyckoff-positions/wyckoff_test.cpp
+++ projects/wyckoff-positions/wyckoff_test.cpp
@@ -1,30 +1,32 @@
+//#include "symop_4d.hpp"
 #include "wyckoff.hpp"
 // is there a way to include all the headers in the avdv-factorgroup folder??
 #include "../../submodules/eigen-git-mirror/Eigen/Core"
 #include "../../submodules/eigen-git-mirror/Eigen/Dense"
 #include "../avdv-factor-group/tests.hpp"
 
-bool test_wyckoff_construction() {
-    Eigen::Vector3d basis_vector1{0,0,1};
-    Eigen::Vector3d basis_vector2{0,0,0};
-    Eigen::Vector3d basis_vector3{1,0,0};
-    Eigen::Vector3d offset{0.25, 0.25,0};
+bool test_wyckoff_construction()
+{
+    Eigen::Vector3d basis_vector1{0, 0, 1};
+    Eigen::Vector3d basis_vector2{0, 0, 0};
+    Eigen::Vector3d basis_vector3{1, 0, 0};
+    Eigen::Vector3d offset{0.25, 0.25, 0};
 
-    Subspace subspace1=Subspace(basis_vector1,basis_vector2, basis_vector3, offset);
+    Subspace subspace1 = Subspace(basis_vector1, basis_vector2, basis_vector3, offset);
     Eigen::Matrix3d expected_basis_matrix;
-    expected_basis_matrix.col(0)=basis_vector1;
-    expected_basis_matrix.col(1)=basis_vector2;
-    expected_basis_matrix.col(2)=basis_vector3;
-    
-    return (expected_basis_matrix==subspace1.basis_col_matrix() && offset==subspace1.offset()); 
+    expected_basis_matrix.col(0) = basis_vector1;
+    expected_basis_matrix.col(1) = basis_vector2;
+    expected_basis_matrix.col(2) = basis_vector3;
+
+    return (expected_basis_matrix == subspace1.basis_col_matrix() && offset == subspace1.offset());
 }
 
-Eigen::Matrix3d make_xyz_rotation_matrix(double degrees) 
+Eigen::Matrix3d make_xyz_rotation_matrix(double degrees)
 {
     Eigen::AngleAxisd rotation_generator(degrees * M_PI / 180.0, Eigen::Vector3d(1, 1, 1).normalized());
     return rotation_generator.matrix();
 }
-Eigen::Matrix3d make_y_rotation_matrix(double degrees) 
+Eigen::Matrix3d make_y_rotation_matrix(double degrees)
 {
     Eigen::AngleAxisd rotation_generator(degrees * M_PI / 180.0, Eigen::Vector3d(0, 1, 0));
     return rotation_generator.matrix();
@@ -64,7 +66,7 @@ PeriodicGroup make_rotation_60_group_diagonal(double tol)
     Lattice identity_lattice(identity_matrix.col(0), identity_matrix.col(1), identity_matrix.col(2));
     BinarySymOpPeriodicCompare_f symop_comparator(identity_lattice, tol);
     BinarySymOpPeriodicMultiplier_f symop_multiplier(identity_lattice, tol);
-    Eigen::Matrix3d rotation_generator_matrix =make_xyz_rotation_matrix(60);
+    Eigen::Matrix3d rotation_generator_matrix = make_xyz_rotation_matrix(60);
     PeriodicGroup rotation_60_group({rotation_generator_matrix}, symop_comparator, symop_multiplier);
     return rotation_60_group;
 }
@@ -195,102 +197,101 @@ bool test_find_invariant_subspace(double tol)
 
 bool test_find_diagonal_axis_invariant_subspace(double tol)
 {
-// TODO: can we test a rotation axis group with diagonal axis??
-    
-    Eigen::Matrix3d expected_basis_col_matrix=Eigen::Matrix3d::Zero();
-    Eigen::Vector3d expected_basis_col=Eigen::Vector3d::Ones().normalized();
-    expected_basis_col_matrix.col(0)=expected_basis_col;
-    PeriodicGroup rotation_60_group_diagonal= make_rotation_60_group_diagonal(tol);
+    // TODO: can we test a rotation axis group with diagonal axis??
+
+    Eigen::Matrix3d expected_basis_col_matrix = Eigen::Matrix3d::Zero();
+    Eigen::Vector3d expected_basis_col = Eigen::Vector3d::Ones().normalized();
+    expected_basis_col_matrix.col(0) = expected_basis_col;
+    PeriodicGroup rotation_60_group_diagonal = make_rotation_60_group_diagonal(tol);
     Subspace invariant_subspace = find_invariant_subspace(rotation_60_group_diagonal);
     std::cout << invariant_subspace.formula() << std::endl;
-    std::cout<<invariant_subspace.formula();
-    //TODO change comparison so the order of the vectors don't matter??
+    std::cout << invariant_subspace.formula();
+    // TODO change comparison so the order of the vectors don't matter??
     return expected_basis_col_matrix.isApprox(invariant_subspace.basis_col_matrix());
 }
 
-
-
 std::vector<Subspace> load_c6_wyckoff_positions()
 {
-    //TODO finish to be used below to check rotation group wyckoff positions. two type, the 00z axis and the 6 xyz types
+    // TODO finish to be used below to check rotation group wyckoff positions. two type, the 00z axis and the 6 xyz
+    // types
     std::vector<Subspace> wyckoff_positions;
     return wyckoff_positions;
 };
 
 bool test_subspace_compare_same(double tol)
-{ 
-//TODO: test subspace comparison function
+{
+    // TODO: test subspace comparison function
 
-    Eigen::Vector3d basis_vector1{0,0,1};
-    Eigen::Vector3d basis_vector2{0,0,0};
-    Eigen::Vector3d basis_vector3{1,0,0};
-    Eigen::Vector3d offset{0.25, 0.25,0};
+    Eigen::Vector3d basis_vector1{0, 0, 1};
+    Eigen::Vector3d basis_vector2{0, 0, 0};
+    Eigen::Vector3d basis_vector3{1, 0, 0};
+    Eigen::Vector3d offset{0.25, 0.25, 0};
 
-    Subspace subspace1=Subspace(basis_vector1,basis_vector2, basis_vector3, offset);
+    Subspace subspace1 = Subspace(basis_vector1, basis_vector2, basis_vector3, offset);
 
     return subspaces_are_equal(subspace1, subspace1, tol);
 }
 
 bool test_subspace_compare_different(double tol)
-{ 
-//TODO: test subspace comparison function
+{
+    // TODO: test subspace comparison function
 
-    Eigen::Vector3d basis_vector1{0,0,1};
-    Eigen::Vector3d basis_vector2{0,0,0};
-    Eigen::Vector3d basis_vector3{1,0,0};
-    Eigen::Vector3d offset{0.25, 0.25,0};
+    Eigen::Vector3d basis_vector1{0, 0, 1};
+    Eigen::Vector3d basis_vector2{0, 0, 0};
+    Eigen::Vector3d basis_vector3{1, 0, 0};
+    Eigen::Vector3d offset{0.25, 0.25, 0};
 
-    Subspace subspace1=Subspace(basis_vector1,basis_vector2, basis_vector3, offset);
-    Subspace subspace2=Subspace(basis_vector2,basis_vector2, basis_vector3, offset);
+    Subspace subspace1 = Subspace(basis_vector1, basis_vector2, basis_vector3, offset);
+    Subspace subspace2 = Subspace(basis_vector2, basis_vector2, basis_vector3, offset);
 
     return !subspaces_are_equal(subspace1, subspace2, tol);
 }
 bool test_subspace_compare_diff_order(double tol)
-{ 
-//TODO: test subspace comparison function
+{
+    // TODO: test subspace comparison function
 
-    Eigen::Vector3d basis_vector1{0,0,1};
-    Eigen::Vector3d basis_vector2{0,0,0};
-    Eigen::Vector3d basis_vector3{1,0,0};
-    Eigen::Vector3d offset{0.25, 0.25,0};
+    Eigen::Vector3d basis_vector1{0, 0, 1};
+    Eigen::Vector3d basis_vector2{0, 0, 0};
+    Eigen::Vector3d basis_vector3{1, 0, 0};
+    Eigen::Vector3d offset{0.25, 0.25, 0};
 
-    Subspace subspace1=Subspace(basis_vector1,basis_vector2, basis_vector3, offset);
-    Subspace subspace2=Subspace(basis_vector2,basis_vector1, basis_vector3, offset);
+    Subspace subspace1 = Subspace(basis_vector1, basis_vector2, basis_vector3, offset);
+    Subspace subspace2 = Subspace(basis_vector2, basis_vector1, basis_vector3, offset);
 
     return subspaces_are_equal(subspace1, subspace2, tol);
 }
 bool test_subspace_compare_equivalent_basis(double tol)
-{ 
-//TODO: test subspace comparison function
+{
+    // TODO: test subspace comparison function
 
-    Eigen::Vector3d basis_vector1{0,0,1};
-    Eigen::Vector3d basis_vector2{0,0,0};
-    Eigen::Vector3d basis_vector3{1,0,0};
-    Eigen::Vector3d basis_vector4{0.5,0,1};
-    Eigen::Vector3d offset{0.25, 0.25,0};
+    Eigen::Vector3d basis_vector1{0, 0, 1};
+    Eigen::Vector3d basis_vector2{0, 0, 0};
+    Eigen::Vector3d basis_vector3{1, 0, 0};
+    Eigen::Vector3d basis_vector4{0.5, 0, 1};
+    Eigen::Vector3d offset{0.25, 0.25, 0};
 
-    Subspace subspace1=Subspace(basis_vector1,basis_vector2, basis_vector3, offset);
-    Subspace subspace2=Subspace(basis_vector1,basis_vector2, basis_vector3, offset);
+    Subspace subspace1 = Subspace(basis_vector1, basis_vector2, basis_vector3, offset);
+    Subspace subspace2 = Subspace(basis_vector1, basis_vector2, basis_vector3, offset);
 
     return subspaces_are_equal(subspace1, subspace2, tol);
 }
 
-
 bool test_find_symmetrically_equivalent_wyckoff_positions(double tol)
 {
-// test for known subgroup without translation
+    // test for known subgroup without translation
 
     PeriodicGroup rotation_60_group = make_rotation_60_group(tol);
     PeriodicGroup rotation_180_group = make_rotation_group(180, tol);
     PeriodicGroup identity_group = make_identity_group(tol);
     std::vector<SymOp> coset_identity = find_coset(rotation_60_group, identity_group);
     std::vector<SymOp> coset_180 = find_coset(rotation_60_group, rotation_180_group);
-    
 
     Subspace wyckoff_prototype = find_invariant_subspace(identity_group);
     Subspace wyckoff_prototype_180 = find_invariant_subspace(rotation_180_group);
-    std::vector<Subspace> wyckoff_e = find_symmetrically_equivalent_wyckoff_positions(coset_identity, wyckoff_prototype);
-    std::vector<Subspace> wyckoff_180 = find_symmetrically_equivalent_wyckoff_positions(coset_180, wyckoff_prototype_180);
+    std::vector<Subspace> wyckoff_e =
+        find_symmetrically_equivalent_wyckoff_positions(coset_identity, wyckoff_prototype);
+    std::vector<Subspace> wyckoff_180 =
+        find_symmetrically_equivalent_wyckoff_positions(coset_180, wyckoff_prototype_180);
 
     if (wyckoff_e.size() != rotation_60_group.operations().size())
     {
@@ -298,40 +299,42 @@ bool test_find_symmetrically_equivalent_wyckoff_positions(double tol)
     }
     std::vector<Subspace> expected_wyckoff_positions = load_c6_wyckoff_positions();
 
-    std::cout<<"identity group wyckoff positions"<<std::endl;
-    for (auto position: wyckoff_e){
-        std::cout<<position.formula()<<std::endl;
+    std::cout << "identity group wyckoff positions" << std::endl;
+    for (auto position : wyckoff_e)
+    {
+        std::cout << position.formula() << std::endl;
     }
 
-    std::cout<< "size of 180 coset:   "<<coset_180.size()<<std::endl;
-    std::cout<<"180 group wyckoff positions"<<std::endl;
-    for (auto position: wyckoff_180){
-        std::cout<<position.formula()<<std::endl;
+    std::cout << "size of 180 coset:   " << coset_180.size() << std::endl;
+    std::cout << "180 group wyckoff positions" << std::endl;
+    for (auto position : wyckoff_180)
+    {
+        std::cout << position.formula() << std::endl;
     }
 
-     /* TODO: check that they are the list of wycoff positions expected (check against list from bilbao with find if)
+    /* TODO: check that they are the list of wycoff positions expected (check against list from bilbao with find if)
      */
     return true;
 }
 
 bool test_wyckoff_position_compare(double tol)
 {
-    /*TODO:Compares vectors of symmetrically equivalent wyckoff positions 
+    /*TODO:Compares vectors of symmetrically equivalent wyckoff positions
      * within tolerance and irrespective of order of positions.
      */
     return false;
 }
 
 WyckoffList load_expected_cubic_point_group_wyckoff_positions()
-{ //TODO: function to generated expected cubic point group
+{ // TODO: function to generated expected cubic point group
     std::vector<std::vector<Subspace>> point_group_wyckoff_list;
     return point_group_wyckoff_list;
 }
 bool test_cubic_point_group_wyckoff_positions(double tol)
-{   
- // TODO:test for simple factor group (ie cubic)
+{
+    // TODO:test for simple factor group (ie cubic)
     /* load simple cubic structure from POSCAR,
-     * generate factor group(same as point group) 
+     * generate factor group(same as point group)
      * generate wyckoff_positions
      * compare number first to expected number of wyckoff positions
      * compare to expected wycoff positions
@@ -343,6 +346,26 @@ bool test_cubic_point_group_wyckoff_positions(double tol)
  * test for harder factor group (hexagonal and diamond)
  */
 
+bool test_sympp_4d_construction()
+{
+    //    Eigen::Matrix3d cart_matrix(2, 0, 0, 0, 2, 0, 0, 0, 2);
+    //    Eigen::Vector3d translation(0.5, 0.5, 0.5);
+    //    Eigen::Matrix4d eigen_4d_matrix(2, 0, 0, 0.5, 0, 2, 0, 0.5, 0, 0, 2, 0.5, 0, 0, 0, 1);
+    //    SymOp sym_op(cart_matrix, translation);
+    //
+    //    Symop_4d symop_4d_matrix_0(cart_matrix, translation);
+    //    Symop_4d symop_4d_matrix_1(eigen_4d_matrix);
+    //    Symop_4d symop_4d_matrix_2(sym_op);
+    //
+    //    //    if (symop_4d_matrix_0.get_matrix().isApprox(eigen_4d_matrix) &&
+    //    //        symop_4d_matrix_1.get_matrix().isApprox(eigen_4d_matrix) &&
+    //    //        symop_4d_matrix_2.get_matrix().isApprox(eigen_4d_matrix))
+    //    //    {
+    //    //        return true;
+    //    //    }
+
+    return false;
+}
 int main()
 {
     double tol = 1e-6;
@@ -352,12 +375,15 @@ int main()
     EXPECT_TRUE(test_find_coset(tol), "Generate Expected Coset");
     EXPECT_TRUE(test_make_reynolds_operator(tol), "Generate 4d Reynolds operator");
     EXPECT_TRUE(test_make_3d_reynolds_operator(tol), "Generate 3d Reynolds operator");
-    EXPECT_TRUE(test_subspace_compare_same(tol), "Subspace compare same subspaces works"); 
-    EXPECT_TRUE(test_subspace_compare_different(tol), "Subspace compare different subspaces works"); 
+    EXPECT_TRUE(test_subspace_compare_same(tol), "Subspace compare same subspaces works");
+    EXPECT_TRUE(test_subspace_compare_different(tol), "Subspace compare different subspaces works");
     EXPECT_TRUE(test_find_invariant_subspace(tol), "Find the expected invariant subspace of subgroup");
-    EXPECT_TRUE(test_find_symmetrically_equivalent_wyckoff_positions(tol), "Find and print symmetrically equivalent wyckoff positions");
-    EXPECT_TRUE(test_find_diagonal_axis_invariant_subspace(tol), "Find the expected invariant subspace of subgroup with diagnol rotation axis");
+    EXPECT_TRUE(test_find_symmetrically_equivalent_wyckoff_positions(tol),
+                "Find and print symmetrically equivalent wyckoff positions");
+    EXPECT_TRUE(test_find_diagonal_axis_invariant_subspace(tol),
+                "Find the expected invariant subspace of subgroup with diagnol rotation axis");
     EXPECT_TRUE(test_wyckoff_position_compare(tol), "Wyckoff Positions Comparison");
     EXPECT_TRUE(test_cubic_point_group_wyckoff_positions(tol), "Generated expected cubic point group");
+    EXPECT_TRUE(test_sympp_4d_construction(), "Test symop4d class");
     return 0;
 }
